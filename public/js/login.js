e61857c67d419ed5d254b16c40988cb5de60e089
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-15 20:36:29 +0200

    supabase

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,48 @@
+<<<<<<< Updated upstream
+=======
+//import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjE0MzQ1NiwiZXhwIjoxOTUxNzE5NDU2fQ.-34dz8ixfgU2k9U1XEin9ETycmRPdGw68XLtdYKTSB8'
+
+const SUPABASE_URL = "https://rwcskqzxmqcotrbcilsz.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
+
+
+let { user, error } = await supabase.auth.signIn({
+    uname: us.value,
+    password: ps.value
+})
+
+
+const em = document.querySelector('#email')
+const ps = document.querySelector('#pass')
+let btn = document.querySelector("#send")
+btn.addEventListener("click",loginIn)
+
+
+const loginIn = (event)=>{
+    event.preventDefault();
+    const email = em.value
+    const pass = ps.value
+    
+    supabase.auth.signIn({email,pass})
+    .then((response)=>{
+        response.error ? alert(response.error.message) : setToken(response)
+    })
+    .catch((err)=>{
+        alert(err.response.text)
+    })
+}
+
+function setToken(response){
+    if(response.data.confirmation_sent_at && !response.data.access_token){
+        alert('Confirmation email sent')
+    }
+}
+ /**********  OLD API *******/
+ /*
+>>>>>>> Stashed changes
 const us = document.querySelector('#uname')
 const ps = document.querySelector('#pass')
 const http = new XMLHttpRequest();
