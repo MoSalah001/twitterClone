00c7d433091d4f287812688acc737df9adcd90b1
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-15 20:24:09 +0200

    testing supabase backend - login

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,58 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = "https://rwcskqzxmqcotrbcilsz.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
+
+
+let { user, error } = await supabase.auth.signIn({
+    uname: us.value,
+    password: ps.value
+})
+
+
+const em = document.querySelector('#email')
+const ps = document.querySelector('#pass')
+let btn = document.querySelector("#send")
+btn.addEventListener("click",loginIn)
+
+
+const loginIn = (event)=>{
+    event.preventDefault();
+    const email = em.value
+    const pass = ps.value
+    
+    supabase.auth.signIn({email,pass})
+    .then((response)=>{
+        response.error ? alert(response.error.message) : setToken(response)
+    })
+    .catch((err)=>{
+        alert(err.response.text)
+    })
+}
+
+function setToken(response){
+    if(response.data.confirmation_sent_at && !response.data.access_token){
+        alert('Confirmation email sent')
+    }
+}
+ /**********  OLD API *******/
+ /*
 const us = document.querySelector('#uname')
 const ps = document.querySelector('#pass')
 const http = new XMLHttpRequest();
 let btn = document.querySelector("#send")
 btn.addEventListener("click",sending)
-
+*/
 function sending(e){
     e.preventDefault();
     var login = {
         uname : us.value,
         pass : ps.value
     }
-        http.open("POST","http://localhost:3000/login")
+        http.open("POST","https://rwcskqzxmqcotrbcilsz.supabase.co")
         http.setRequestHeader('content-type','application/json')
+        http.setRequestHeader()
         http.send(JSON.stringify(login));
         http.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
