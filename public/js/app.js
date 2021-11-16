e001dbf308474fd540f704ecc871e0f796aedb42
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-16 19:45:11 +0200

    test ...

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,35 @@
-const supabaseUrl = 'https://rwcskqzxmqcotrbcilsz.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from './node_modules/@supabase/supabase-js/dist/module/index.js'
+
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjE0MzQ1NiwiZXhwIjoxOTUxNzE5NDU2fQ.-34dz8ixfgU2k9U1XEin9ETycmRPdGw68XLtdYKTSB8'
+
+const SUPABASE_URL = "https://rwcskqzxmqcotrbcilsz.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+document.addEventListener("DOMContentLoaded",function (event) {
+
+  const em = document.querySelector("#email")
+  const ps = document.querySelector("#pass")
+
+  const logIn = (event)=> {
+    supabase.auth
+    .signIn({em , ps})
+    .then((response)=>{
+      response.error ? alert(response.error.message) : setToken(response)
+    })
+    .catch((err)=>{
+      alert(err.response.text)
+    })
+  }
+
+  function setToken(response) {
+    if (response.data.confirmation_sent_at && !response.data.access.token){
+      alert("Confirmation Email sent!")
+    }else {
+      alert("Logged in as "+response.user.email)
+    }
+  }
+
+})
+
 
