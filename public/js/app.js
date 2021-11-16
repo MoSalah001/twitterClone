c9efb41eed409fae6f1a9173856920cf144e1316
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-16 21:34:48 +0200

    testing ...

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-import 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1.28.0/dist/umd/supabase.min.js'
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1.28.0/dist/umd/supabase.min.js'
 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjE0MzQ1NiwiZXhwIjoxOTUxNzE5NDU2fQ.-34dz8ixfgU2k9U1XEin9ETycmRPdGw68XLtdYKTSB8'
 
