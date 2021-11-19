123bae7cca3cad393fc72b2b33b9f2cacb57e87d
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-19 23:00:12 +0200

    ....S

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,23 @@
-import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1.28.0/dist/umd/supabase.min.js'
+var _superbasejs = require("@supabase/supabase-js")
+document.addEventListener("DOMContentLoaded",function (event) {
+//const {createClient} = supabase
+
 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjE0MzQ1NiwiZXhwIjoxOTUxNzE5NDU2fQ.-34dz8ixfgU2k9U1XEin9ETycmRPdGw68XLtdYKTSB8'
 
 const SUPABASE_URL = "https://rwcskqzxmqcotrbcilsz.supabase.co"
 
+let btn = document.querySelector("#send")
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-document.addEventListener("DOMContentLoaded",function (event) {
 
   const em = document.querySelector("#email")
   const ps = document.querySelector("#pass")
 
   const logIn = (event)=> {
-    supabase.auth
-    .signIn({em , ps})
+    console.log(this);
+     supabase.auth.signIn({em , ps})
     .then((response)=>{
       response.error ? alert(response.error.message) : setToken(response)
     })
@@ -29,7 +33,7 @@ document.addEventListener("DOMContentLoaded",function (event) {
       alert("Logged in as "+response.user.email)
     }
   }
-
+  btn.addEventListener("click",logIn)
 })
 
 
