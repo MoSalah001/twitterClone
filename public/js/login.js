504b84c267dcba40f6a5e4453c44b679a90d6c0a
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-26 14:41:36 +0200

    fix error message on login

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ function sending(e){
         http.setRequestHeader('content-type','application/json')
         http.send(JSON.stringify(login));
         http.onreadystatechange = function(){
-            if(this.readyState == 3 && this.status == 200){
+            if(this.readyState == 4 && this.status == 200){
                 console.log("first if");
                 let data = JSON.parse(this.response)
                 const uid = {id:data.id}
@@ -25,17 +25,6 @@ function sending(e){
             }
             else if (this.status == 404) {
                 console.log("first else if");
-                document.querySelector('#err').textContent =this.responseText
-            }
-            else if(this.readyState == 4){
-                console.log("second else if"); 
-                let data = JSON.parse(this.response)
-                const uid = {id:data.id}
-               window.localStorage.setItem('ID',uid.id)
-               return window.location = data.url
-            }
-            else {
-                console.log("the else statement");
                 document.querySelector('#err').textContent =this.responseText
             }
         }
