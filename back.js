dca2c4715354e1121f0cb34a4ec942e0dc3b8b68
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-26 14:13:44 +0200

    fix error message on login

--- back.js
+++ back.js
@@ -109,13 +109,15 @@ app.post("/login",(req,res)=>{
         pass = result.rows[0].pass
         bcrypt.compare(password,pass,(err,isMatch)=>{
           if(err){
-            console.log(password);
             res.status(404).send("you entered a wrong password, please try again")
           }
           if(isMatch) {
             let data ={
               id:result.rows[0].user_id,
               url:loc+'./js/main.html'
+            }
+            if(!isMatch){
+              res.status(404).send("you entered a wrong password, please try again")
             }
             res.send(data)
           }
