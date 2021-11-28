dacc7976b8d0406539cf24c5f70b84e10d77d945
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-28 23:37:56 +0200

    setting event listner

--- public/js/main.js
+++ public/js/main.js
@@ -76,6 +76,9 @@ feeding.onreadystatechange=()=>{
             div.append(svg)
             div.append(p)
             feed.append(div)
+
+            let deleteBtn = document.getElementById("#deleteBtn")
+            deleteBtn.addEventListener("click",deleteTweet())
         }
     }
 }
