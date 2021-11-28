1fcf11917d19505477c8b4cd0dfbd11853330c6d
MoSalah001 <mhmdsalah707@gmail.com>
2021-11-28 23:27:10 +0200

    setting event listner

--- public/js/tweetFunctions.js
+++ public/js/tweetFunctions.js
@@ -1,3 +1,7 @@
+let deleteBtn = document.querySelector("deleteBtn")
+deleteBtn.addEventListener("click",deleteTweet())
 function deleteTweet(){
-    
+    let uid = window.localStorage.getItem(ID)
+    let tweetId = this
+    console.log(this);
 }
